f884b98f867db140112eb44c795b9f11cf1d65e2
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-11-09 12:03:37 +0300

    create clickable component

---
+++ src/components/Clickable/index.tsx
@@ -0,0 +1,38 @@
+import './style.css';
+
+type CursorType = 'pointer' | 'crosshair' | 'zoom-in' | 'wait' | 'not-allowed';
+
+type Props = {
+  children: JSX.Element;
+  style?: any;
+  onClick: () => any;
+  cursor?: CursorType;
+  loading?: boolean;
+  enabled?: boolean;
+};
+
+const Clickable: React.FC<Props> = ({
+  children,
+  style,
+  onClick,
+  cursor = 'pointer',
+  loading = false,
+  enabled = true,
+}: Props) => {
+  return (
+    <button
+      disabled={!enabled}
+      onClick={() => !loading && onClick()}
+      className="Clickable"
+      style={{
+        cursor: !enabled ? 'no-drop' : loading ? 'wait' : cursor,
+        ...style,
+        opacity: loading ? 0.7 : 1,
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Clickable;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import { palette } from 'palette';
-import { Button, Space, P, Input } from 'components';
+import { Button, Space, P, Input, Clickable } from 'components';
 import { h, w } from 'windowDimensions';
 
 type ReduxProps = unknown;
@@ -13,9 +13,22 @@ type Props = ReduxProps & HomeProps;
 
 const Home: React.FC<Props> = () => {
   const [inputV, setInputV] = useState('');
+  const [loading, setLoading] = useState(false);
   return (
     <div style={{ width: w(100), height: h(100) }}>
       <Input title={'Password'} placeholder={'enter your passord'} value={inputV} onChange={setInputV} />
+      <Clickable
+        loading={loading}
+        onClick={() => {
+          setLoading(true);
+        }}
+      >
+        <img
+          src={
+            'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+          }
+        />
+      </Clickable>
     </div>
   );
 };
