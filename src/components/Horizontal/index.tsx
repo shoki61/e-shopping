42270dc25079db5b81bcce5388765daf6676d3bc
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-11-10 14:41:32 +0300

    create horizontal component

---
+++ src/components/Horizontal/index.tsx
@@ -0,0 +1,51 @@
+import './style.css';
+
+type Props = {
+  align?: 'top' | 'middle' | 'bottom';
+  justify?: 'start' | 'center' | 'end';
+  children: JSX.Element[];
+  style?: React.CSSProperties;
+  wrap?: boolean;
+  spread?: boolean;
+};
+
+type AlignAndJustifyType = {
+  [key: string]: string;
+};
+
+const alignItemsValue: AlignAndJustifyType = {
+  top: 'flex-start',
+  middle: 'center',
+  bottom: 'flex-end',
+};
+
+const justifyContentValue: AlignAndJustifyType = {
+  start: 'flex-start',
+  center: 'center',
+  end: 'flex-end',
+};
+
+const Horizontal: React.FC<Props> = ({
+  align = 'top',
+  justify = 'start',
+  children,
+  wrap = false,
+  spread = false,
+  style,
+}: Props) => {
+  return (
+    <div
+      className={'Horizontal'}
+      style={{
+        alignItems: alignItemsValue[align],
+        justifyContent: spread ? 'space-between' : justifyContentValue[justify],
+        flexWrap: wrap ? 'wrap' : 'nowrap',
+        ...style,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Horizontal;
