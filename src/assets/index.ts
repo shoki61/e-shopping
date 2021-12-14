9d22851b27de6fa033a844a3538f75bf12a288da
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-12-14 17:30:09 +0300

    refactor clickable component

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,7 @@
 export { default as AppLogo } from './e-shopping.svg';
-export { default as EmptyCart } from './empty-cart.svg';
 export { default as Bat } from './bat.png';
 export { default as Camera } from './camera.png';
 export { default as Empty } from './empty.png';
+export { default as EmptyCart } from './empty-cart.png';
 
 export * from './flags';
